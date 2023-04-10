a79d7523d485487d8e2d5bac4b0daa6c094f6336
testsnake <testsnake12@gmail.com>
2023-04-09 23:18:30 -0700

    Update ButtonManager.java

--- src/main/java/org/bcit/com2522/project/scuffed/uicomponents/ButtonManager.java
+++ src/main/java/org/bcit/com2522/project/scuffed/uicomponents/ButtonManager.java
@@ -4,49 +4,51 @@ import java.util.ArrayList;
 import org.bcit.com2522.project.scuffed.client.Window;
 
 /**
- * The type Button manager.
+ * Represents a ButtonManager that handles the addition, removal, drawing, and
+ * management of multiple Button objects.
  */
 public class ButtonManager {
   /**
-   * The Buttons.
+   * The collection of Button objects.
    */
   public ArrayList<Button> buttons = new ArrayList<Button>();
+
   /**
-   *  The scene.
+   * The Window scene where the buttons are displayed.
    */
   Window scene;
 
   /**
-   * Instantiates a new Button manager.
+   * Constructs a new ButtonManager associated with the given Window scene.
    *
-   * @param scene the scene
+   * @param scene the Window scene where the buttons will be displayed
    */
   public ButtonManager(Window scene) {
     this.scene = scene;
   }
 
   /**
-   * Adds a button.
+   * Adds a new Button to the ButtonManager.
    *
-   * @param button the button
+   * @param button the Button to add
    */
   public void add(Button button) {
     buttons.add(button);
   }
 
   /**
-   * Removes a specific button.
+   * Removes a specific Button from the ButtonManager.
    *
-   * @param button the button
+   * @param button the Button to remove
    */
   public void remove(Button button) {
     buttons.remove(button);
   }
 
   /**
-   * Draws all buttons.
+   * Draws all buttons managed by the ButtonManager on the specified Window scene.
    *
-   * @param scene the scene
+   * @param scene the Window scene where the buttons will be drawn
    */
   public void draw(Window scene) {
     for (Button button : buttons) {
@@ -55,7 +57,7 @@ public class ButtonManager {
   }
 
   /**
-   * Deletes all buttons.
+   * Deletes all buttons managed by the ButtonManager.
    */
   public void wipe() {
     buttons.clear();
